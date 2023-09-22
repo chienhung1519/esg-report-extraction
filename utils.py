f1d449d14719aa769c9e84203d1f1f444ff3eb8c
chienhung1519 <chienhung1519@gmail.com>
2023-09-22 07:41:49 +0000

    parse report to db

---
+++ utils.py
@@ -0,0 +1,17 @@
+from  langchain.schema import Document
+import json
+from typing import Iterable
+
+def save_docs_to_jsonl(array:Iterable[Document], file_path:str) -> None:
+    with open(file_path, "w") as jsonl_file:
+        for doc in array:
+            jsonl_file.write(doc.json(ensure_ascii=False) + "\n")
+
+def load_docs_from_jsonl(file_path:str) -> Iterable[Document]:
+    array = []
+    with open(file_path, "r") as jsonl_file:
+        for line in jsonl_file:
+            data = json.loads(line)
+            obj = Document(**data)
+            array.append(obj)
+    return array
